ac2e6c3ff94c1024f3b204958b9331add0ae6ab7
randomOmodnar <53683707+randomOmodnar@users.noreply.github.com>
2020-07-16 09:05:18 +0800

    part 1 未使用模板

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world????")
+
+
+def detail(requests, question_id):
+    return HttpResponse('you are looking at question %s'%question_id)
+
+
+def results(requests, question_id):
+    return HttpResponse('you are looking at the results of question %s' % question_id)
+
+
+def vote(requests, question_id):
+    return HttpResponse('you are voting on question %s' % question_id)
