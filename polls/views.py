946f7110b305ac07c8e8cd9ac402097a66f7293c
randomOmodnar <53683707+randomOmodnar@users.noreply.github.com>
2020-07-16 09:35:09 +0800

    part 2, 使用模板

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
+from django.shortcuts import render
 
 
+# 显示所有'投票问题'列表
 def index(request):
-    return HttpResponse("Hello, world????")
+    """render: 载入模板， 填充上下文"""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
-def detail(requests, question_id):
-    return HttpResponse('you are looking at question %s'%question_id)
+def detail(request, question_id):
+    """存在问题 Question 没有object属性"""
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(requests, question_id):
